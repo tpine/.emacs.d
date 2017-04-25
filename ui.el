58ef4cba515ea92dd7a432ba06cbee2ddb2846bd
Thomas <thomas@pinegrove.io>
2017-04-25 22:02:26 +1000

    Remove menubar and scrollbar from ui

--- ui.el
+++ ui.el
@@ -14,6 +14,8 @@
 (global-linum-mode t)
 (setq column-number-mode t)
 (tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 (custom-set-variables '(custom-safe-themes t))
 
 (use-package spacemacs-theme
