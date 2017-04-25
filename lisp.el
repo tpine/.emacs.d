fa686454033bb4d4941bddffb96326c1ca5d07a8
Thomas <thomas@pinegrove.io>
2017-04-25 22:00:51 +1000

    Remove Slime and replace with Sly

--- lisp.el
+++ lisp.el
@@ -7,36 +7,28 @@
 ;;; Add .lsp files to lisp-mode
 (add-to-list 'auto-mode-alist '("\\.lsp" . lisp-mode))
 
+(use-package sly
+  :ensure t
+  :config
+  (setq inferior-lisp-program "sbcl")
+  (setq sly-auto-start 'ask))
+
 (use-package rainbow-delimiters
   :ensure t
   :init
-  (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-  (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode))
+  (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'sly-mrepl-mode-hook #'rainbow-delimiters-mode))
 
 (use-package paredit
   :ensure t
   :init
-  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
   (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode))
-
-(use-package slime
-  :ensure t
-  :config
-  (add-to-list 'load-path "~/.emacs.d/slime-contrib/slime")
-  (setq inferior-lisp-program "sbcl")
-  (setq slime-auto-start 'ask)
-  (setq slime-contribs '(slime-fancy
-			 slime-mrepl
-			 slime-banner
-			 slime-tramp
-			 slime-xref-browser
-			 slime-highlight-edits
-			 slime-sprof))
-  (add-hook 'slime-repl-mode-hook       #'enable-paredit-mode)
-  (add-hook 'slime-repl-mode-hook 'rainbow-delimiters-mode))
+  (add-hook 'ielm-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'sly-mrepl-mode-hook #'enable-paredit-mode))
 
 (provide 'lisp)
 ;;; lisp.el ends here
