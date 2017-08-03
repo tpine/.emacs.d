a6e7fb7e3be63e9c04d59ac184dd2658f494d1dd
Thomas <thomas@pinegrove.io>
2017-08-03 13:36:37 +1000

    Fixup web config
    - Add smartparens instead of paredit
    - Move js2-mode-hooks into js2-mode use-package

--- web.el
+++ web.el
@@ -13,20 +13,24 @@
   ;; This will pull in js2 as well
   :ensure t)
 
+(use-package smartparens
+  :ensure t
+  :init
+  (use-package smartparens-config)
+  (smartparens-global-mode 1))
+
 (use-package js2-mode
   :ensure t
   :init
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+    (add-hook 'js2-mode-hook 'company-mode)
+    (add-hook 'js2-mode-hook 'rainbow-delimiters-mode)))
 
 (use-package company-tern
   :ensure t
   :init
   (add-to-list 'company-backends 'company-tern))
-
-;;; We also want paredit and rainbow-delimiters to load
-(add-hook 'js2-mode-hook 'company-mode)
-(add-hook 'js2-mode-hook 'paredit-mode)
-(add-hook 'js2-mode-hook 'rainbow-delimiters-mode)
 
 (provide 'web)
 ;;; web.el ends here
