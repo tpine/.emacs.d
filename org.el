e45718d62d6da33b747c46c12190dc56d0a7d71d
Thomas <thomas@pinegrove.io>
2017-11-04 10:16:32 +1100

    Add Elfeed

--- org.el
+++ org.el
@@ -12,5 +12,14 @@
 			       "~/org/home.org")
 	org-enforce-todo-dependencies t))
 
+(use-package elfeed
+  :ensure t)
+
+(use-package elfeed-org
+  :ensure t
+  :config
+  (elfeed-org)
+  (setq rmh-elfeed-org-files (list "~/org/feeds.org")))
+
 (provide 'org)
 ;;; org.el ends here
