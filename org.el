0a4062893163694bc65b78d36fa9666d3267f781
Thomas <thomas@pinegrove.io>
2017-11-05 14:06:53 +1100

    Setup Org Babel

--- org.el
+++ org.el
@@ -8,9 +8,23 @@
   :ensure t
   :config
   ;; Set global todo list
-  (setq org-agenda-files (list "~/org/home.org")
-	org-todo-keywords '((sequence "TODO" "|" "DONE" "CANCELED"))
-	org-enforce-todo-dependencies t))
+  (progn
+    ;; active Babel languages
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((R . t)
+       (emacs-lisp . t)
+       (shell . t)
+       (lisp . t)
+       (latex . t)
+       (php . t)
+       (js . t)
+       (mongo . t)))
+    (setq org-babel-lisp-eval-fn "sly-eval")
+    ;; General Org Config
+    (setq org-agenda-files (list "~/org/home.org")
+	  org-todo-keywords '((sequence "TODO" "|" "DONE" "CANCELED"))
+	  org-enforce-todo-dependencies t)))
 
 (use-package elfeed
   :ensure t)
