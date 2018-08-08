a1a4be17efc00a38f4332c01951c213655132ba0
Thomas Atkinson <thomas.atkinson@blacknova.com.au>
2018-08-08 10:43:42 +1000

    Format Email Propeerly With Signiture before send

--- email.el
+++ email.el
@@ -2,6 +2,24 @@
 ;;; Commentary:
 
 ;;; Code:
+
+(defun format-email ()
+  (interactive)
+  (beginning-of-buffer)
+  (search-forward "--text follows this line--")
+  (next-line nil)
+  (message-beginning-of-line nil)
+  (set-mark-command nil)
+  (search-forward "--")
+  (previous-line nil)
+  (move-end-of-line nil)
+  (org-mime-htmlize)
+  (set-mark-command nil)
+  (search-backward "<#/multipart>\n<#/multipart>")
+  (kill-region (point) (mark))
+  (end-of-buffer)
+  (insert "<#/multipart><#/multipart>"))
+
 (use-package notmuch
   :ensure t
   :config
@@ -11,7 +29,8 @@
 	mail-envelope-from 'header
 	notmuch-address-command 'internal)
   (add-hook 'notmuch-message-mode-hook 'flyspell-mode)
-  (add-hook 'notmuch-message-mode-hook 'flyspell-buffer))
+  (add-hook 'notmuch-message-mode-hook 'flyspell-buffer)
+  (add-hook 'message-send-hook 'format-email))
 
 
 
