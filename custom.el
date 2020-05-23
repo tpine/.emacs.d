6cbd286df7d06debb4082af672ffe5fffe4c9b31
Thomas Atkinson <tnatkinson95@gmail.com>
2020-05-24 07:09:17 +1000

    Add Custom File

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes t)
+ '(package-selected-packages
+   '(nginx-mode elfeed-protocol org-journal apache-mode anyins dockerfile-mode yaml-mode company-terraform terraform-mode docker phpcbf company-php ac-php tide xref-js2 js2-refactor js2-mode prettier-js add-node-modules-path company-tern web-mode notmuch elfeed-org elfeed org-plus-contrib ob-http auctex paredit sly dired-narrow diredfl smartparens ranger multiple-cursors ace-window avy yasnippet-snippets yasnippet undo-tree move-text rainbow-delimiters expand-region forge magit company counsel ivy flycheck nyan-mode doom-themes exec-path-from-shell diminish use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight bold :height 98 :width normal)))))
