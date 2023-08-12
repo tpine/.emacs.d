6cc36e9728611e9ce95cd374608c2aeb2fcf63d9
Thomas Atkinson <tnatkinson95@gmail.com>
2023-08-12 17:02:28 +1000

    Remove Org Elpa

--- init.el
+++ init.el
@@ -24,7 +24,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (when (boundp 'package-pinned-packages)
   (setq package-pinned-packages
         '((org-plus-contrib . "org"))))
