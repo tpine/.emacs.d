597b1cdf9d917512ee01222e56d6b8e75681edb0
Thomas Atkinson <tnatkinson95@gmail.com>
2024-04-20 16:18:56 +1000

    Selected Packages

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit counsel ivy nyan-mode doom-themes)))
+ '(package-selected-packages
+   '(smartparens rainbow-delimiters terraform-mode undo-tree lsp-mode rustic magit counsel ivy nyan-mode doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
