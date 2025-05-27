c194031c343cf261195a817559303e756e35b7b0
Thomas Atkinson <tnatkinson95@gmail.com>
2025-05-27 12:22:37 +1000

    custom.el bump

--- custom.el
+++ custom.el
@@ -3,8 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-babel-load-languages '((emacs-lisp . t) (shell . t)))
  '(package-selected-packages
-   '(dired-narrow expand-region tsx-mode prettier-js lsp-ui web-mode smartparens rainbow-delimiters terraform-mode undo-tree lsp-mode rustic magit counsel ivy nyan-mode doom-themes)))
+   '(arduino-mode auctex counsel dired-narrow dockerfile-mode doom-themes
+		  expand-region go-mode indent-bars ivy jsonnet-mode
+		  k8s-mode kubernetes lsp-mode lsp-ui magit nyan-mode
+		  org-journal powershell prettier-js
+		  rainbow-delimiters rustic shfmt smartparens
+		  terraform-mode tsx-mode undo-tree vue-mode web-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
